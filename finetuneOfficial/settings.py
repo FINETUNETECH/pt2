16f75210a0d0cb01b4a52162a24e9922ed5f2a7f
FINETUNETECH <finetunetech.e@gmail.com>
2025-03-06 16:01:48 +0530

    Update Vercel deployment configuration

--- finetuneOfficial/settings.py
+++ finetuneOfficial/settings.py
@@ -172,9 +172,15 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Change this from 'static' to 'staticfiles'
 
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'user.CustomUser'
@@ -188,7 +194,6 @@ AUTHENTICATION_BACKENDS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
-    'https://phonetreat-git-main-finetunetechs-projects.vercel.app',
     'https://phonetreat-git-preview-finetunetechs-projects.vercel.app',
     'https://*.vercel.app',
     'https://phonetreat.in',
